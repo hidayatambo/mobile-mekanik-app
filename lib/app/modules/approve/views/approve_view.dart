e90ba4db46edd24dc9f939854ff0f4a25f575497
irwan44 <irwanstwn2398@gmail.com>
2024-04-23 18:23:02 +0700

    push ahh

--- lib/app/modules/approve/views/approve_view.dart
+++ lib/app/modules/approve/views/approve_view.dart
@@ -39,65 +39,15 @@ class ApproveView extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                FutureBuilder<Boking>(
-                  future: API.bokingid(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return CircularProgressIndicator();
-                    } else if (snapshot.hasError) {
-                      return Text('Error: ${snapshot.error}');
-                    } else {
-                      List<DataBooking>? bookings = snapshot.data?.dataBooking;
-                      print('Data dari API: $bookings');
-
-                      if (bookings == null || bookings.isEmpty) {
-                        // Tampilkan pesan jika tidak ada data yang diterima
-                        return Text('Tidak ada data yang diterima dari server');
-                      }
-
-                      // Tambahkan variabel untuk menunjukkan apakah ada data yang sesuai
-                      bool dataFound = false;
-
-                      for (DataBooking e in bookings) {
-                        if (e.status != null && e.namaJenissvc != null) {
-                          if (e.status!.toLowerCase() == 'diproses' &&
-                              e.namaJenissvc!.toLowerCase() != 'general check up/p2h') {
-                            // Lakukan navigasi jika ada data yang sesuai
-                            Get.toNamed(
-                              Routes.APPROVE,
-                              arguments: {
-                                'id': e.id.toString(),
-                                'tgl_booking': e.tglBooking.toString(),
-                                'jam_booking': e.jamBooking.toString(),
-                                'nama': e.nama.toString(),
-                                'nama_jenissvc': e.namaJenissvc.toString(),
-                                'no_polisi': e.noPolisi.toString(),
-                                'nama_merk': e.namaMerk.toString(),
-                                'nama_tipe': e.namaTipe.toString(),
-                                'status': e.status.toString(),
-                              },
-                            );
-                            // Set dataFound menjadi true karena ada data yang sesuai
-                            dataFound = true;
-                            // Keluar dari loop karena sudah menemukan data yang sesuai
-                            break;
-                          }
-                        }
-                      }
-
-                      if (!dataFound) {
-                        // Tampilkan pesan jika tidak ada data yang sesuai
-                        return Text('Tidak ada data yang sesuai');
-                      } else {
-                        // Jika ada data yang sesuai, tidak perlu menampilkan widget apa pun di sini
-                        // Karena navigasi telah dilakukan di dalam loop
-                        return SizedBox();
-                      }
-                    }
-                  },
-                ),
-
-
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      elevation: 4.0,
+                    ),
+                    child: const Text('Approve',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),)),
 
                 const SizedBox(width: 10,),
             ElevatedButton(

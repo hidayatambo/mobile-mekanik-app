d6cb7df9c295f921fbf3af6d87386c100c475a40
irwan44 <irwanstwn2398@gmail.com>
2024-05-13 17:34:15 +0700

    eda

--- lib/app/modules/general_checkup/views/percobaan.dart
+++ lib/app/modules/general_checkup/views/percobaan.dart
@@ -19,7 +19,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    selectedItem = 'Pilih';
+    selectedItem = '';
   }
 
   @override

c44cd4b55fe627f39178e1b0e8fac5eca3ffb61e
irwan44 <irwanstwn2398@gmail.com>
2024-05-29 17:20:43 +0700

    mantap

---
+++ lib/app/modules/promek/start_stop/start_stop_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StartStopView extends StatefulWidget {
+  const StartStopView({super.key});
+
+  @override
+  State<StartStopView> createState() => _StartStopViewState();
+}
+
+class _StartStopViewState extends State<StartStopView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

2e094f007333ef889520e363dfd8dee86c89e957
hidayatambo <hidayatsyahidin5@gmail.com>
2024-06-15 21:35:41 +0700

    alhamdulillah ya allah

--- lib/app/modules/promek/detail/card_detailpkb.dart
+++ lib/app/modules/promek/detail/card_detailpkb.dart
@@ -1,190 +1,169 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import '../../../data/data_endpoint/detailhistory.dart';
+import '../../../data/data_endpoint/mekanik_pkb.dart';
+import '../../../data/endpoint.dart';
+import '../controllers/promek_controller.dart';
 
-import '../../../data/data_endpoint/pkb.dart';
-
-class CardDetailPKB extends StatelessWidget {
+class CardDetailPKB extends StatefulWidget {
   const CardDetailPKB({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final Map<String, dynamic> args =
-    ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+  State<CardDetailPKB> createState() => _CardDetailPKBState();
+}
 
-    // Accessing jasa and parts lists
-    final List<dynamic>? jasa = args['jasa'];
-    final List<dynamic>? parts = args['parts'];
+class _CardDetailPKBState extends State<CardDetailPKB> {
+  final PromekController controller = Get.put(PromekController());
+  late RefreshController _refreshController;
+
+  @override
+  void initState() {
+    _refreshController = RefreshController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Map args = Get.arguments;
 
     return Container(
-      padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.15),
-            spreadRadius: 5,
-            blurRadius: 10,
-            offset: const Offset(0, 3),
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.15),
+                spreadRadius: 5,
+                blurRadius: 10,
+                offset: const Offset(0, 3),
+              ),
+            ],
           ),
-        ],
-      ),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              args['tipe_svc'] ?? '',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-                fontSize: 18,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                args['tipe_svc'] ?? '',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Example color
+                  fontSize: 18,
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            _buildInfoRow('Tanggal & Jam PKB:', args['tgl_pkb'] ?? ''),
-            _buildInfoRow('Jam Selesai:', args['jam_selesai'] ?? '-'),
-            _buildInfoRow('Cabang:', args['nama_cabang'] ?? ''),
-            _buildInfoRow('Kode PKB:', args['kode_pkb'] ?? '',
-                color: Colors.green),
-            _buildInfoRow('Tipe Pelanggan:', args['tipe_pelanggan'] ?? ''),
-            const Divider(color: Colors.grey),
-            const SizedBox(height: 10),
-            Text(
-              'Detail Pelanggan',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-                fontSize: 16,
+              const SizedBox(height: 10),
+              _buildInfoRow('Tanggal & Jam PKB:', args['tgl_pkb'] ?? ''),
+              _buildInfoRow('Jam Selesai:', args['jam_selesai'] ?? '-'),
+              _buildInfoRow('Cabang:', args['nama_cabang'] ?? ''),
+              _buildInfoRow('Kode PKB:', args['kode_pkb'] ?? '',
+                  color: Colors.green),
+              _buildInfoRow('Tipe Pelanggan:', args['tipe_pelanggan'] ?? ''),
+              const Divider(color: Colors.grey),
+              const SizedBox(height: 10),
+              Text(
+                'Detail Pelanggan',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Example color
+                  fontSize: 16,
+                ),
               ),
-            ),
-            _buildDetailText('Nama:', args['nama'] ?? ''),
-            _buildDetailText('No Handphone:', args['telp'] ?? ''),
-            _buildDetailText('Alamat:', args['alamat'] ?? ''),
-            const Divider(color: Colors.grey),
-            const SizedBox(height: 10),
-            Text(
-              'Kendaraan Pelanggan',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-                fontSize: 16,
+              _buildDetailText('Nama:', args['nama'] ?? ''),
+              _buildDetailText('No Handphone:', args['telp'] ?? ''),
+              _buildDetailText('Alamat:', args['alamat'] ?? ''),
+              const Divider(color: Colors.grey),
+              const SizedBox(height: 10),
+              Text(
+                'Kendaraan Pelanggan',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Example color
+                  fontSize: 16,
+                ),
               ),
-            ),
-            _buildInfoRow('Merk:', args['nama_merk'] ?? ''),
-            _buildInfoRow('Tipe:', args['nama_tipe'] ?? ''),
-            _buildInfoRow('Tahun:', args['tahun'] ?? '-'),
-            _buildInfoRow('Warna:', args['warna'] ?? '-'),
-            _buildInfoRow('Kategori Kendaraan:',
-                args['kategori_kendaraan'] ?? '-'),
-            _buildInfoRow('Transmisi:', args['transmisi'] ?? '-'),
-            _buildInfoRow('No Polisi:', args['no_polisi'] ?? '-'),
-            const Divider(color: Colors.grey),
-            _buildDetailSection('Keluhan:', args['keluhan'] ?? '-'),
-            const Divider(color: Colors.grey),
-            const SizedBox(height: 10),
-            Text(
-              'Parts',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-                fontSize: 16,
+              _buildInfoRow('Merk:', args['nama_merk'] ?? ''),
+              _buildInfoRow('Tipe:', args['nama_tipe'] ?? ''),
+              _buildInfoRow('Tahun:', args['tahun'] ?? '-'),
+              _buildInfoRow('Warna:', args['warna'] ?? '-'),
+              _buildInfoRow('Kategori Kendaraan:', args['kategori_kendaraan'] ?? '-'),
+              _buildInfoRow('Transmisi:', args['transmisi'] ?? '-'),
+              _buildInfoRow('No Polisi:', args['no_polisi'] ?? '-'),
+              const Divider(color: Colors.grey),
+              _buildDetailSection('Keluhan:', args['keluhan'] ?? '-'),
+              const Divider(color: Colors.grey),
+              const SizedBox(height: 10),
+              Text(
+                'Jasa',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Example color
+                  fontSize: 16,
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            _buildPartsList(parts?.cast<Parts>()),
-            const SizedBox(height: 10),
-            Text(
-              'Jasa',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-                fontSize: 16,
+              const SizedBox(height: 10),
+              FutureBuilder<MekanikPKB>(
+                future: API.MeknaikPKBID(kodesvc: args['kode_svc'] ?? ''),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else {
+                    final jasaList = snapshot.data?.dataJasaMekanik?.jasa ?? [];
+                    if (jasaList.isEmpty) {
+                      return Container(
+                        height: 200,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Belum ada Jasa',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red, // Example color
+                          ),
+                        ),
+                      );
+                    }
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: jasaList.length,
+                      itemBuilder: (context, index) {
+                        final jasa = jasaList[index];
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              jasa.namaJasa ?? '',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Text('Harga Jasa:  ${formatCurrency(jasa.hargaJasa)}',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                )),
+                            const Divider(),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                },
               ),
-            ),
-            const SizedBox(height: 10),
-            _buildJasaList(jasa?.cast<Jasa>()),
-          ],
-        ),
-      ),
+            ],
+          ),
     );
   }
-
-  Widget _buildJasaList(List<Jasa>? jasaItems) {
-    if (jasaItems == null || jasaItems.isEmpty) {
-      return Container(
-        height: 100,
-        alignment: Alignment.center,
-        child: Text(
-          'Belum ada Jasa',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.red,
-          ),
-        ),
-      );
+  String formatCurrency(int? amount) {
+    if (amount == null) {
+      return 'Rp. -'; // or any default value you prefer for null case
     }
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: jasaItems.map((jasa) => Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '${jasa.namaJasa}',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Text(
-            'Qty: ${jasa.qtyJasa}',
-            style: TextStyle(fontWeight: FontWeight.normal),
-          ),
-          Text(
-            'Harga: ${formatCurrency(jasa.hargaJasa ?? 0)}',
-          ),
-          Divider(color: Colors.grey),
-        ],
-      )).toList(),
-    );
-  }
-  Widget _buildPartsList(List<Parts>? partsItems) {
-    if (partsItems == null || partsItems.isEmpty) {
-      return Container(
-        height: 100,
-        alignment: Alignment.center,
-        child: Text(
-          'Belum ada Parts',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.red,
-          ),
-        ),
-      );
-    }
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: partsItems.map((part) => Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '${part.namaSparepart} - Qty: ${part.qty}',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Text(
-            'Qty: ${part.qty}',
-            style: TextStyle(fontWeight: FontWeight.normal),
-          ),
-          Text(
-            'Harga: ${formatCurrency(part.hargaSparepart ?? 0)}',
-          ),
-          Divider(color: Colors.grey),
-        ],
-      )).toList(),
-    );
-  }
-
-  String formatCurrency(int amount) {
     var format = NumberFormat.currency(locale: 'id_ID', symbol: 'Rp');
     return format.format(amount);
   }
@@ -242,8 +221,8 @@ class CardDetailPKB extends StatelessWidget {
           ),
         ),
         Container(
-          padding: const EdgeInsets.all(5),
           width: double.infinity,
+          padding: const EdgeInsets.all(5),
           decoration: BoxDecoration(
             border: Border.all(color: Colors.grey),
             borderRadius: BorderRadius.circular(5),
@@ -251,7 +230,7 @@ class CardDetailPKB extends StatelessWidget {
           child: Text(
             value,
             style: TextStyle(
-              fontWeight: FontWeight.bold,
+              fontWeight: FontWeight.normal,
             ),
           ),
         ),
